4115c1b7ccb5e387a1170145db3930a7a02e10b6
me <mathieu.minguez@gmail.com>
2023-04-27 12:33:53 +0200

    Add and configure Routes

--- Kasa/src/main.jsx
+++ Kasa/src/main.jsx
@@ -5,17 +5,38 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from './App.jsx'
+import Home from './routes/Home';
+import About from './routes/About';
+import Fiche from './routes/Fiche';
+import NotFound from './routes/NotFound';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about/",
+        element: <About />,
+      },
+      {
+        path: "fiche/:ficheId",
+        element: <Fiche />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <App />
   </React.StrictMode>,
 )
---
+++ Kasa/src/routes/Fiche.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Fiche() {
+  return <h1>Fiche Page</h1>;
+}
+
+export default Fiche;
